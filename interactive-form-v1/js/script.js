c512eef648f8d9365ee19393cf6e5d8391cd23ea
Austin Scott <austin.scott18@gmail.com>
2019-02-08 08:25:08 -0500

    "Other" job role hides when not selected

--- interactive-form-v1/js/script.js
+++ interactive-form-v1/js/script.js
@@ -1,1 +1,13 @@
-$('input').first().focus(); //When page loads, set focus to first text field.
+$(document).ready(function(){
+  $('input').first().focus(); //When page loads, set focus to first text field.
+});
+
+$("#other-title").hide();
+
+$("#title").on('change', function (event) {
+  if(event.target.value === "other") {
+    $("#other-title").show();
+  } else {
+    $("#other-title").hide();
+  }
+});
